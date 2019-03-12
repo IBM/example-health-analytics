6a327a95a1e6bf2fce3e15dae412ae4e1b047803
Max Shapiro <MaxShapiro32@ibm.com>
2019-03-12 15:32:59 -0400

    Altered if statements

--- travis-tests/docker-compose.sh
+++ travis-tests/docker-compose.sh
@@ -4,12 +4,14 @@ source "$(dirname "$0")"/../pattern-ci/scripts/resources.sh
 main(){
     if ! docker-compose up -d; then
         test_failed "$0"
-    elif ! docker-compose ps; then
-        test_failed "$0"
-    elif ! cd generate && ./generate.sh; then
-        test_failed "$0"
-    else
-        test_passed "$0"
     fi
+    if ! docker-compose ps; then
+        test_failed "$0"
+    fi
+    if ! cd generate && ./generate.sh; then
+        test_failed "$0"
+    fi
+    
+    test_passed "$0"
 }
 main "$@" 
