8bd181631badf0c6297732907759118400e81d1f
Max Shapiro <MaxShapiro32@ibm.com>
2019-03-12 13:37:03 -0400

    Initial Travis CI

---
+++ travis-tests/docker-compose.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+# shellcheck disable=SC1090
+source "$(dirname "$0")"/../pattern-ci/scripts/resources.sh
+main(){
+    if ! docker-compose up -d; then
+        test_failed "$0"
+    elif ! docker-compose ps; then
+        test_failed "$0"
+    elif ! sleep 1 && cd generate && ./generate.sh; then
+        test_failed "$0"
+    else
+        test_passed "$0"
+    fi
+}
+main "$@" 
