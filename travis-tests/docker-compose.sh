2374b3264502d7ba37f4dacee41aeeecf6fd6118
Max Shapiro <MaxShapiro32@ibm.com>
2019-03-12 15:27:35 -0400

    Removed sleep

--- travis-tests/docker-compose.sh
+++ travis-tests/docker-compose.sh
@@ -1,19 +1,12 @@
 #!/bin/bash -e
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../pattern-ci/scripts/resources.sh
-
-generate_data(){
-	if ! sleep 1 && cd generate && ./generate.sh; then
-        test_failed "$0"
-    fi
-}
-
 main(){
     if ! docker-compose up -d; then
         test_failed "$0"
     elif ! docker-compose ps; then
         test_failed "$0"
-    elif ! generate_data; then
+    elif ! cd generate && ./generate.sh; then
         test_failed "$0"
     else
         test_passed "$0"
