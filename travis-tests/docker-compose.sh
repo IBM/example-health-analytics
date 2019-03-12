8b482016e99679b6414ed6a7edc2c73bfaedc116
Max Shapiro <MaxShapiro32@ibm.com>
2019-03-12 15:24:05 -0400

    Modified Travis test

--- travis-tests/docker-compose.sh
+++ travis-tests/docker-compose.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 # shellcheck disable=SC1090
 source "$(dirname "$0")"/../pattern-ci/scripts/resources.sh
+
+generate_data(){
+	if ! sleep 1 && cd generate && ./generate.sh; then
+        test_failed "$0"
+    fi
+}
+
 main(){
     if ! docker-compose up -d; then
         test_failed "$0"
     elif ! docker-compose ps; then
         test_failed "$0"
-    elif ! sleep 1 && cd generate && ./generate.sh; then
+    elif ! generate_data; then
         test_failed "$0"
     else
         test_passed "$0"
