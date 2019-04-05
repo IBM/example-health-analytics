2efc0d9640bb5f39b596460356ac5a1c957c4b7e
Max Shapiro <MaxShapiro32@ibm.com>
2019-04-05 10:21:09 -0400

    Added loader

--- web/public/javascripts/mapHandler.js
+++ web/public/javascripts/mapHandler.js
@@ -11,120 +11,121 @@
  */
 function updateMap(dataType, allergy, dataValueType) {
 
-    mapboxgl.accessToken = properties.mapboxAccessToken;
+    return new Promise(function(resolve,reject) {
 
-    getData(dataType, allergy, dataValueType).then(mapData => {
+        mapboxgl.accessToken = properties.mapboxAccessToken;
 
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/antonmc/cjr27i9iw0wbq2rnwtvkrbgog',
-            center: [-110.5467, 46.0731],
-            zoom: 2.5,
-            minZoom: 2.5,
-            maxZoom: 14
-        });
+        getData(dataType, allergy, dataValueType).then(mapData => {
 
+            var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/antonmc/cjr27i9iw0wbq2rnwtvkrbgog',
+                center: [-110.5467, 46.0731],
+                zoom: 2.5,
+                minZoom: 2.5,
+                maxZoom: 14
+            });
 
+            map.on('load', function () {
 
+                var heatmapMin = 1;
+                var heatmapMax = 1;
 
-        map.on('load', function () {
-
-            var heatmapMin = 1;
-            var heatmapMax = 1;
-
-            switch (dataValueType) {
-                case "total":
-                    for (var data = 0; data < mapData.length; data++) {
-                        if (heatmapMax < mapData[data].properties.data) {
-                            heatmapMax = mapData[data].properties.data;
+                switch (dataValueType) {
+                    case "total":
+                        for (var data = 0; data < mapData.length; data++) {
+                            if (heatmapMax < mapData[data].properties.data) {
+                                heatmapMax = mapData[data].properties.data;
+                            }
                         }
-                    }
 
-                    break;
-                case "percentage":
-                    heatmapMax = 100;
-                    break;
-                default:
-            }
-
-            map.addLayer({
-                "id": "heat",
-                "type": "heatmap",
-                "source": {
-                    "type": "geojson",
-                    "data": {
-                        "type": "FeatureCollection",
-                        "features": mapData
-                    }
-                },
-                "paint": {
-                // Increase the heatmap weight based on frequency and property magnitude
-                "heatmap-weight": [
-                "interpolate",
-                ["linear"],
-                ["get", "data"],
-                0, 0,
-                heatmapMax, heatmapMin
-                ],
-                // Increase the heatmap color weight weight by zoom level
-                // heatmap-intensity is a multiplier on top of heatmap-weight
-                "heatmap-intensity": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                0, 1,
-                9, 3
-                ],
-                // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-                // Begin color ramp at 0-stop with a 0-transparancy color
-                // to create a blur-like effect.
-                "heatmap-color": [
-                "interpolate",
-                ["linear"],
-                ["heatmap-density"],
-                0, "rgba(149,214,198,0)",
-                0.2, "rgb(252,210,188)",
-                0.4, "rgb(253,237,228)",
-                0.6, "rgb(253,219,199)",
-                0.8, "rgb(239,138,98)",
-                1, "rgb(249,143,88)"
-                ],
-                // Adjust the heatmap radius by zoom level
-                "heatmap-radius": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                0, 2,
-                9, 100
-                ],
+                        break;
+                    case "percentage":
+                        heatmapMax = 100;
+                        break;
+                    default:
                 }
-                }, 'waterway-label');
 
-            map.addLayer({
-                "id": "points",
-                "type": "symbol",
-                "source": {
-                    "type": "geojson",
-                    "data": {
-                        "type": "FeatureCollection",
-                        "features": mapData
+                map.addLayer({
+                    "id": "heat",
+                    "type": "heatmap",
+                    "source": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": mapData
+                        }
+                    },
+                    "paint": {
+                    // Increase the heatmap weight based on frequency and property magnitude
+                    "heatmap-weight": [
+                    "interpolate",
+                    ["linear"],
+                    ["get", "data"],
+                    0, 0,
+                    heatmapMax, heatmapMin
+                    ],
+                    // Increase the heatmap color weight weight by zoom level
+                    // heatmap-intensity is a multiplier on top of heatmap-weight
+                    "heatmap-intensity": [
+                    "interpolate",
+                    ["linear"],
+                    ["zoom"],
+                    0, 1,
+                    9, 3
+                    ],
+                    // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+                    // Begin color ramp at 0-stop with a 0-transparancy color
+                    // to create a blur-like effect.
+                    "heatmap-color": [
+                    "interpolate",
+                    ["linear"],
+                    ["heatmap-density"],
+                    0, "rgba(149,214,198,0)",
+                    0.2, "rgb(252,210,188)",
+                    0.4, "rgb(253,237,228)",
+                    0.6, "rgb(253,219,199)",
+                    0.8, "rgb(239,138,98)",
+                    1, "rgb(249,143,88)"
+                    ],
+                    // Adjust the heatmap radius by zoom level
+                    "heatmap-radius": [
+                    "interpolate",
+                    ["linear"],
+                    ["zoom"],
+                    0, 2,
+                    9, 100
+                    ],
                     }
-                },
-                "layout": {
-                    "text-field": "{title}",
-                    "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-                    "text-anchor": "top"
-                },
-                "paint": {
-                    "icon-opacity": [
-                        "interpolate",
-                        ["linear"],
-                        ["zoom"],
-                        7, 0,
-                        8, 1
-                        ]
-                }
+                    }, 'waterway-label');
+
+                map.addLayer({
+                    "id": "points",
+                    "type": "symbol",
+                    "source": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": mapData
+                        }
+                    },
+                    "layout": {
+                        "text-field": "{title}",
+                        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                        "text-anchor": "top"
+                    },
+                    "paint": {
+                        "icon-opacity": [
+                            "interpolate",
+                            ["linear"],
+                            ["zoom"],
+                            7, 0,
+                            8, 1
+                            ]
+                    }
+                });
+                resolve(true);
             });
         });
-    });
+    })
 }
